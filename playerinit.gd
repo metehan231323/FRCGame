5cbe11669abfdd7079ec943b0f726e90148908d6
metehan231323 <metehancelikoyuncu@gmail.com>
2026-01-07 15:06:31 +0300

    safe

--- playerinit.gd
+++ playerinit.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-@export var speed : int = 85;
+@export var speed : int = 85  ;
 @export var gravity : int = 300;
 @export var jumpforce : int = 200;
 @export var hasDoubleJump : bool = false;
